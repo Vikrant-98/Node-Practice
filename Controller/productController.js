85d6affa06a465edeae046eab37ba16ee0dd8ec2
vikrantchitte5398 <vikrantchitte5398@gmail.com>
2026-07-12 01:49:44 +0530

    Added : Product crud operations

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,9 +1,46 @@
 
 const Product = require('../Model/products');
 
-exports.getProduct =  (request, response, next) => {
-    console.log("this is product middleware");
-    const products = Product.findAll().then(products => {
+// exports.getProduct =  (request, response, next) => {
+//     console.log("this is product middleware");
+//     const products = Product.findAll().then(products => {
+//         return products;
+//     }).then(products => {
+//         console.log(products);
+//         response.json(products);
+//     }).catch(err => {
+//         console.log(err);
+//     });
+// };
+
+exports.getProduct = async (req, res, next) => {
+    try {
+        // Default values
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+
+        const offset = (page - 1) * limit;
+
+        const products = await Product.findAll({
+            limit,
+            offset,
+            order: [['id', 'ASC']] // Recommended for consistent pagination
+        });
+
+        res.status(200).json(products);
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+};
+
+exports.getProductById = (request, response, next) => {
+    var productId = request.params.productId;
+    const products = Product.findAll( { where: { id: productId } })
+    .then(products => {
         return products;
     }).then(products => {
         console.log(products);
@@ -12,6 +49,7 @@ exports.getProduct =  (request, response, next) => {
         console.log(err);
     });
 };
+
 
 exports.createProduct = async (request, response, next) => {
     console.log("this is product post middleware");
@@ -28,4 +66,70 @@ exports.createProduct = async (request, response, next) => {
     }).catch(err => {
         console.log(err);
     });    
+};
+
+exports.updateProduct = async (req, res) => {
+    try {
+
+        const id = req.params.id;
+
+        const product = await Product.findByPk(id).then(product => {
+            
+            var { title, imageUrl, description, price } = req.body;
+            if (title) product.title = title;
+            if (imageUrl) product.imageUrl = imageUrl;
+            if (description) product.description = description;
+            if (price) product.price = price;
+
+            return product.save();
+
+        })
+        .then(updatedProduct => {
+            return res.status(200).json({
+            message: "Product updated successfully"
+        });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+       
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    try {
+
+        const id = req.params.id;
+
+        Product.findByPk(id)
+            .then(product => {
+
+                if (!product) {
+                    return res.status(404).json({
+                        message: "Product not found"
+                    });
+                }
+
+                product.destroy();
+                return res.status(200).json({
+                    message: "Product deleted successfully"
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json({
+                    message: err.message
+                });
+            });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: err.message
+        });
+    }
 };
--- Routes/product.js
+++ Routes/product.js
@@ -1,8 +1,11 @@
 const espress = require('express');
 const router = espress.Router();
-const { getProduct, createProduct } = require('../Controller/productController');
+const { getProduct, createProduct, getProductById , updateProduct, deleteProduct } = require('../Controller/productController');
 
 router.get('/', getProduct);
+router.get('/:productId', getProductById);
 router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
